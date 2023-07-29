a8385dd4a746f996c29b523cde695042a3bdf95a
Erick Omondi <eriqueomondi1997@gmail.com>
2023-07-29 18:59:30 +0300

    Weather App

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+
 //FUNCTION TO UPDATE THE DETAILS FROM RESPECTIVE API
 async function checkWeather(city){
   const response = await fetch(apiUrl + city + `&appid=${apikey}`);
   var data = await response.json();
 
-  console.log(data);
-//targeting the city, temperature, humidity and wind speed class names and updating them based on the API
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-//updating the images displaying weather
+  //Error Code
+  if(response.status == 404){
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    //updating the images displaying weather
   if(data.weather[0].main == "Clouds"){
     weatherIcon.src = "";
   } else if (data.weather[0].main == "Clear"){
@@ -29,6 +28,20 @@ async function checkWeather(city){
   } else if (data.weather[0].main == "Mist"){
     weatherIcon.src = "";
   }
+
+  document.querySelector(".weather").style.display = "block";
+  }
+
+  console.log(data);
+//targeting the city, temperature, humidity and wind speed class names and updating them based on the API
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+  document.querySelector(".error").style.display = "none";
+  document.querySelector(".weather").style.display = "block";
 }
 
 //linking the search button with the search input field
